9223a6d7a992fe0586e20d9fa13b9786a56947a5
Sakshi <sakshiguptalohiya2000@gmail.com>
2023-03-24 14:52:54 +0530

    second commit

--- src/main/java/com/jwtuser/model/CustomerUserDetails.java
+++ src/main/java/com/jwtuser/model/CustomerUserDetails.java
@@ -38,7 +38,7 @@ public class CustomerUserDetails implements UserDetails {
         return user.getUsername();
     }
 
-    
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
